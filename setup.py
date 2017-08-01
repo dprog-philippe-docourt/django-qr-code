a98301a98f1a3a154f5ca88837e2a89695332dc0
philippe-docourt <philippe@docourt.ch>
2017-08-02 00:34:10 +0200

    Several fixes to setup.py. Removed useless files.

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='django-qr-code',
     version='0.1.0',
-    packages=['qr_code', 'qr_code.migrations'],
+    packages=['qr_code', 'qr_code.templatetags'],
     url='https://github.com/dprog-philippe-docourt/django-qr-code',
     license='BSD 3-clause',
     author='Philippe Docourt',
     author_email='philippe@docourt.ch',
-    description='An application that provides tools for displaying QR codes on your Django site.'
+    description='An application that provides tools for displaying QR codes on your Django site.',
+    install_requires=['qrcode', 'django'],
+    python_requires='>=3',
 )
