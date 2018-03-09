df796d7a5b9c961fbed523f43ab49c02b1ea0dab
philippe.docourt <philippe@docourt.ch>
2018-03-09 22:38:49 +0100

    Disabling cache for QR codes for applications was not possible. Rewrite code to make it easier to add new QR code options.

--- qr_code/templatetags/qr_code.py
+++ qr_code/templatetags/qr_code.py
@@ -8,106 +8,124 @@ from qr_code.qr_code import make_embedded_qr_code, make_contact_text, make_wifi_
 register = template.Library()
 
 
-@register.simple_tag()
-def qr_from_text(text, size=DEFAULT_MODULE_SIZE, border=DEFAULT_BORDER_SIZE, version=DEFAULT_VERSION, image_format=DEFAULT_IMAGE_FORMAT):
-    return make_embedded_qr_code(text, size=size, border=border, version=version, image_format=image_format)
+def _make_qr_code(text, qr_code_options, embedded):
+    if 'size' not in qr_code_options:
+        qr_code_options['size'] = DEFAULT_MODULE_SIZE
+    if 'border' not in qr_code_options:
+        qr_code_options['border'] = DEFAULT_BORDER_SIZE
+    if 'version' not in qr_code_options:
+        qr_code_options['version'] = DEFAULT_VERSION
+    if 'image_format' not in qr_code_options:
+        qr_code_options['image_format'] = DEFAULT_IMAGE_FORMAT
+    if embedded:
+        return make_embedded_qr_code(text, **qr_code_options)
+    else:
+        if 'cache_enabled' not in qr_code_options:
+            qr_code_options['cache_enabled'] = DEFAULT_CACHE_ENABLED
+        return make_qr_code_url(text, **qr_code_options)
 
 
 @register.simple_tag()
-def qr_for_email(email, size=DEFAULT_MODULE_SIZE, border=DEFAULT_BORDER_SIZE, version=DEFAULT_VERSION, image_format=DEFAULT_IMAGE_FORMAT):
-    return make_embedded_qr_code(make_email_text(email), size=size, border=border, version=version, image_format=image_format)
+def qr_from_text(text, **kwargs):
+    """Wrapper for function :func:`~qr_code.qr_code._make_qr_code`"""
+    return _make_qr_code(text, qr_code_options=kwargs, embedded=True)
 
 
 @register.simple_tag()
-def qr_for_tel(phone_number, size=DEFAULT_MODULE_SIZE, border=DEFAULT_BORDER_SIZE, version=DEFAULT_VERSION, image_format=DEFAULT_IMAGE_FORMAT):
-    return make_embedded_qr_code(make_tel_text(phone_number), size=size, border=border, version=version, image_format=image_format)
+def qr_for_email(email, **kwargs):
+    return _make_qr_code(make_email_text(email), qr_code_options=kwargs, embedded=True)
 
 
 @register.simple_tag()
-def qr_for_sms(phone_number, size=DEFAULT_MODULE_SIZE, border=DEFAULT_BORDER_SIZE, version=DEFAULT_VERSION, image_format=DEFAULT_IMAGE_FORMAT):
-    return make_embedded_qr_code(make_sms_text(phone_number), size=size, border=border, version=version, image_format=image_format)
+def qr_for_tel(phone_number, **kwargs):
+    return _make_qr_code(make_tel_text(phone_number), qr_code_options=kwargs, embedded=True)
 
 
 @register.simple_tag()
-def qr_for_geolocation(latitude, longitude, altitude, size=DEFAULT_MODULE_SIZE, border=DEFAULT_BORDER_SIZE, version=DEFAULT_VERSION, image_format=DEFAULT_IMAGE_FORMAT):
-    return make_embedded_qr_code(make_geolocation_text(latitude=latitude, longitude=longitude, altitude=altitude), size=size, border=border, version=version, image_format=image_format)
+def qr_for_sms(phone_number, **kwargs):
+    return _make_qr_code(make_sms_text(phone_number), qr_code_options=kwargs, embedded=True)
 
 
 @register.simple_tag()
-def qr_for_google_maps(latitude, longitude, size=DEFAULT_MODULE_SIZE, border=DEFAULT_BORDER_SIZE, version=DEFAULT_VERSION, image_format=DEFAULT_IMAGE_FORMAT):
-    return make_embedded_qr_code(make_google_maps_text(latitude=latitude, longitude=longitude), size=size, border=border, version=version, image_format=image_format)
+def qr_for_geolocation(latitude, longitude, altitude, **kwargs):
+    return _make_qr_code(make_geolocation_text(latitude, longitude, altitude), qr_code_options=kwargs, embedded=True)
 
 
 @register.simple_tag()
-def qr_for_youtube(video_id, size=DEFAULT_MODULE_SIZE, border=DEFAULT_BORDER_SIZE, version=DEFAULT_VERSION, image_format=DEFAULT_IMAGE_FORMAT):
-    return make_embedded_qr_code(make_youtube_text(video_id), size=size, border=border, version=version, image_format=image_format)
+def qr_for_google_maps(latitude, longitude, **kwargs):
+    return _make_qr_code(make_google_maps_text(latitude=latitude, longitude=longitude), qr_code_options=kwargs, embedded=True)
 
 
 @register.simple_tag()
-def qr_for_google_play(package_id, size=DEFAULT_MODULE_SIZE, border=DEFAULT_BORDER_SIZE, version=DEFAULT_VERSION, image_format=DEFAULT_IMAGE_FORMAT):
-    return make_embedded_qr_code(make_google_play_text(package_id), size=size, border=border, version=version, image_format=image_format)
+def qr_for_youtube(video_id, **kwargs):
+    return _make_qr_code(make_youtube_text(video_id), qr_code_options=kwargs, embedded=True)
 
 
 @register.simple_tag()
-def qr_for_contact(contact_dict, size=DEFAULT_MODULE_SIZE, border=DEFAULT_BORDER_SIZE, version=DEFAULT_VERSION, image_format=DEFAULT_IMAGE_FORMAT):
+def qr_for_google_play(package_id, **kwargs):
+    return _make_qr_code(make_google_play_text(package_id), qr_code_options=kwargs, embedded=True)
+
+
+@register.simple_tag()
+def qr_for_contact(contact_dict, **kwargs):
     contact_as_mecard = make_contact_text(contact_dict=contact_dict)
-    return make_embedded_qr_code(contact_as_mecard, size=size, border=border, version=version, image_format=image_format)
+    return _make_qr_code(contact_as_mecard, qr_code_options=kwargs, embedded=True)
 
 
 @register.simple_tag()
-def qr_for_wifi(wifi_dict, size=DEFAULT_MODULE_SIZE, border=DEFAULT_BORDER_SIZE, version=DEFAULT_VERSION, image_format=DEFAULT_IMAGE_FORMAT):
+def qr_for_wifi(wifi_dict, **kwargs):
     wifi_config = make_wifi_text(wifi_dict=wifi_dict)
-    return make_embedded_qr_code(wifi_config, size=size, border=border, version=version, image_format=image_format)
+    return _make_qr_code(wifi_config, qr_code_options=kwargs, embedded=True)
 
 
 @register.simple_tag()
-def qr_url_from_text(text, size=DEFAULT_MODULE_SIZE, border=DEFAULT_BORDER_SIZE, version=DEFAULT_VERSION, image_format=DEFAULT_IMAGE_FORMAT, cache_enabled=DEFAULT_CACHE_ENABLED):
+def qr_url_from_text(text, **kwargs):
     """Wrapper for function :func:`~qr_code.qr_code.make_qr_code_url`"""
-    return make_qr_code_url(text, size=size, border=border, version=version, image_format=image_format, cache_enabled=cache_enabled)
+    return _make_qr_code(text, qr_code_options=kwargs, embedded=False)
 
 
 @register.simple_tag()
-def qr_url_for_email(email, size=DEFAULT_MODULE_SIZE, border=DEFAULT_BORDER_SIZE, version=DEFAULT_VERSION, image_format=DEFAULT_IMAGE_FORMAT):
-    return make_qr_code_url(make_email_text(email), size=size, border=border, version=version, image_format=image_format)
+def qr_url_for_email(email, **kwargs):
+    return _make_qr_code(make_email_text(email), qr_code_options=kwargs, embedded=False)
 
 
 @register.simple_tag()
-def qr_url_for_tel(phone_number, size=DEFAULT_MODULE_SIZE, border=DEFAULT_BORDER_SIZE, version=DEFAULT_VERSION, image_format=DEFAULT_IMAGE_FORMAT):
-    return make_qr_code_url(make_tel_text(phone_number), size=size, border=border, version=version, image_format=image_format)
+def qr_url_for_tel(phone_number, **kwargs):
+    return _make_qr_code(make_tel_text(phone_number), qr_code_options=kwargs, embedded=False)
 
 
 @register.simple_tag()
-def qr_url_for_sms(phone_number, size=DEFAULT_MODULE_SIZE, border=DEFAULT_BORDER_SIZE, version=DEFAULT_VERSION, image_format=DEFAULT_IMAGE_FORMAT):
-    return make_qr_code_url(make_sms_text(phone_number), size=size, border=border, version=version, image_format=image_format)
+def qr_url_for_sms(phone_number, **kwargs):
+    return _make_qr_code(make_sms_text(phone_number), qr_code_options=kwargs, embedded=False)
 
 
 @register.simple_tag()
-def qr_url_for_geolocation(latitude, longitude, altitude, size=DEFAULT_MODULE_SIZE, border=DEFAULT_BORDER_SIZE, version=DEFAULT_VERSION, image_format=DEFAULT_IMAGE_FORMAT):
-    return make_qr_code_url(make_geolocation_text(latitude=latitude, longitude=longitude, altitude=altitude), size=size, border=border, version=version, image_format=image_format)
+def qr_url_for_geolocation(latitude, longitude, altitude, **kwargs):
+    return _make_qr_code(make_geolocation_text(latitude=latitude, longitude=longitude, altitude=altitude), qr_code_options=kwargs, embedded=False)
 
 
 @register.simple_tag()
-def qr_url_for_google_maps(latitude, longitude, size=DEFAULT_MODULE_SIZE, border=DEFAULT_BORDER_SIZE, version=DEFAULT_VERSION, image_format=DEFAULT_IMAGE_FORMAT):
-    return make_qr_code_url(make_google_maps_text(latitude=latitude, longitude=longitude), size=size, border=border, version=version, image_format=image_format)
+def qr_url_for_google_maps(latitude, longitude, **kwargs):
+    return _make_qr_code(make_google_maps_text(latitude=latitude, longitude=longitude), qr_code_options=kwargs, embedded=False)
 
 
 @register.simple_tag()
-def qr_url_for_youtube(video_id, size=DEFAULT_MODULE_SIZE, border=DEFAULT_BORDER_SIZE, version=DEFAULT_VERSION, image_format=DEFAULT_IMAGE_FORMAT):
-    return make_qr_code_url(make_youtube_text(video_id), size=size, border=border, version=version, image_format=image_format)
+def qr_url_for_youtube(video_id, **kwargs):
+    return _make_qr_code(make_youtube_text(video_id), qr_code_options=kwargs, embedded=False)
 
 
 @register.simple_tag()
-def qr_url_for_google_play(package_id, size=DEFAULT_MODULE_SIZE, border=DEFAULT_BORDER_SIZE, version=DEFAULT_VERSION, image_format=DEFAULT_IMAGE_FORMAT):
-    return make_qr_code_url(make_google_play_text(package_id), size=size, border=border, version=version, image_format=image_format)
+def qr_url_for_google_play(package_id, **kwargs):
+    return _make_qr_code(make_google_play_text(package_id), qr_code_options=kwargs, embedded=False)
 
 
 @register.simple_tag()
-def qr_url_for_contact(contact_dict, size=DEFAULT_MODULE_SIZE, border=DEFAULT_BORDER_SIZE, version=DEFAULT_VERSION, image_format=DEFAULT_IMAGE_FORMAT):
+def qr_url_for_contact(contact_dict, **kwargs):
     contact_as_mecard = make_contact_text(contact_dict=contact_dict)
-    return make_qr_code_url(contact_as_mecard, size=size, border=border, version=version, image_format=image_format)
+    return _make_qr_code(contact_as_mecard, qr_code_options=kwargs, embedded=False)
 
 
 @register.simple_tag()
-def qr_url_for_wifi(wifi_dict, size=DEFAULT_MODULE_SIZE, border=DEFAULT_BORDER_SIZE, version=DEFAULT_VERSION, image_format=DEFAULT_IMAGE_FORMAT):
+def qr_url_for_wifi(wifi_dict, **kwargs):
     wifi_config = make_wifi_text(wifi_dict=wifi_dict)
-    return make_qr_code_url(wifi_config, size=size, border=border, version=version, image_format=image_format)
+    return _make_qr_code(wifi_config, qr_code_options=kwargs, embedded=False)
