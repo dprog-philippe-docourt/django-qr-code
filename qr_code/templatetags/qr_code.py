843cddde0ea1f9d916c50ed40503682a4bc87e16
philippe.docourt <philippe@docourt.ch>
2018-03-16 00:09:20 +0100

    Cleanup documentation.

--- qr_code/templatetags/qr_code.py
+++ qr_code/templatetags/qr_code.py
@@ -47,119 +47,99 @@ def _make_geolocation_qr_code(coordinates, embedded, qr_code_args):
 
 @register.simple_tag()
 def qr_from_text(text, **kwargs):
-    """Wrapper for function :func:`~qr_code.qrcode.make_qr_code`"""
     return _make_qr_code(text, qr_code_args=kwargs, embedded=True)
 
 
 @register.simple_tag()
 def qr_for_email(email, **kwargs):
-    """Wrapper for function :func:`~qr_code.qrcode.maker.make_embedded_qr_code`"""
     return _make_qr_code(make_email_text(email), qr_code_args=kwargs, embedded=True)
 
 
 @register.simple_tag()
 def qr_for_tel(phone_number, **kwargs):
-    """Wrapper for function :func:`~qr_code.qrcode.maker.make_embedded_qr_code`"""
     return _make_qr_code(make_tel_text(phone_number), qr_code_args=kwargs, embedded=True)
 
 
 @register.simple_tag()
 def qr_for_sms(phone_number, **kwargs):
-    """Wrapper for function :func:`~qr_code.qrcode.maker.make_embedded_qr_code`"""
     return _make_qr_code(make_sms_text(phone_number), qr_code_args=kwargs, embedded=True)
 
 
 @register.simple_tag()
 def qr_for_geolocation(latitude, longitude, altitude, **kwargs):
-    """Wrapper for function :func:`~qr_code.qrcode.maker.make_embedded_qr_code`"""
     return _make_geolocation_qr_code(_Coordinates(latitude, longitude, altitude), qr_code_args=kwargs, embedded=True)
 
 
 @register.simple_tag()
 def qr_for_google_maps(latitude, longitude, **kwargs):
-    """Wrapper for function :func:`~qr_code.qrcode.maker.make_embedded_qr_code`"""
     return _make_google_maps_qr_code(_Coordinates(latitude, longitude), embedded=True, qr_code_args=kwargs)
 
 
 @register.simple_tag()
 def qr_for_youtube(video_id, **kwargs):
-    """Wrapper for function :func:`~qr_code.qrcode.maker.make_embedded_qr_code`"""
     return _make_qr_code(make_youtube_text(video_id), qr_code_args=kwargs, embedded=True)
 
 
 @register.simple_tag()
 def qr_for_google_play(package_id, **kwargs):
-    """Wrapper for function :func:`~qr_code.qrcode.maker.make_embedded_qr_code`"""
     return _make_qr_code(make_google_play_text(package_id), qr_code_args=kwargs, embedded=True)
 
 
 @register.simple_tag()
 def qr_for_contact(contact_detail, **kwargs):
-    """Wrapper for function :func:`~qr_code.qrcode.maker.make_embedded_qr_code`"""
     return make_contact_or_wifi_qr_code(contact_detail, ContactDetail, qr_code_args=kwargs, embedded=True)
 
 
 @register.simple_tag()
 def qr_for_wifi(wifi_config, **kwargs):
-    """Wrapper for function :func:`~qr_code.qrcode.maker.make_embedded_qr_code`"""
     return make_contact_or_wifi_qr_code(wifi_config, WifiConfig, qr_code_args=kwargs, embedded=True)
 
 
 @register.simple_tag()
 def qr_url_from_text(text, **kwargs):
-    """Wrapper for function :func:`~qr_code.qrcode.serve.make_qr_code_url`"""
     return _make_qr_code(text, qr_code_args=kwargs, embedded=False)
 
 
 @register.simple_tag()
 def qr_url_for_email(email, **kwargs):
-    """Wrapper for function :func:`~qr_code.qrcode.serve.make_qr_code_url`"""
     return _make_qr_code(make_email_text(email), qr_code_args=kwargs, embedded=False)
 
 
 @register.simple_tag()
 def qr_url_for_tel(phone_number, **kwargs):
-    """Wrapper for function :func:`~qr_code.qrcode.serve.make_qr_code_url`"""
     return _make_qr_code(make_tel_text(phone_number), qr_code_args=kwargs, embedded=False)
 
 
 @register.simple_tag()
 def qr_url_for_sms(phone_number, **kwargs):
-    """Wrapper for function :func:`~qr_code.qrcode.serve.make_qr_code_url`"""
     return _make_qr_code(make_sms_text(phone_number), qr_code_args=kwargs, embedded=False)
 
 
 @register.simple_tag()
 def qr_url_for_geolocation(latitude, longitude, altitude, **kwargs):
-    """Wrapper for function :func:`~qr_code.qrcode.serve.make_qr_code_url`"""
     return _make_geolocation_qr_code(_Coordinates(latitude, longitude, altitude), qr_code_args=kwargs, embedded=False)
 
 
 @register.simple_tag()
 def qr_url_for_google_maps(latitude, longitude, **kwargs):
-    """Wrapper for function :func:`~qr_code.qrcode.serve.make_qr_code_url`"""
     return _make_google_maps_qr_code(_Coordinates(latitude, longitude), qr_code_args=kwargs, embedded=False)
 
 
 @register.simple_tag()
 def qr_url_for_youtube(video_id, **kwargs):
-    """Wrapper for function :func:`~qr_code.qrcode.serve.make_qr_code_url`"""
     return _make_qr_code(make_youtube_text(video_id), qr_code_args=kwargs, embedded=False)
 
 
 @register.simple_tag()
 def qr_url_for_google_play(package_id, **kwargs):
-    """Wrapper for function :func:`~qr_code.qrcode.serve.make_qr_code_url`"""
     return _make_qr_code(make_google_play_text(package_id), qr_code_args=kwargs, embedded=False)
 
 
 @register.simple_tag()
 def qr_url_for_contact(contact_detail, **kwargs):
-    """Wrapper for function :func:`~qr_code.qrcode.serve.make_qr_code_url`"""
     return make_contact_or_wifi_qr_code(contact_detail, ContactDetail, qr_code_args=kwargs, embedded=False)
 
 
 @register.simple_tag()
 def qr_url_for_wifi(wifi_config, **kwargs):
-    """Wrapper for function :func:`~qr_code.qrcode.serve.make_qr_code_url`"""
     return make_contact_or_wifi_qr_code(wifi_config. WifiConfig, qr_code_args=kwargs, embedded=False)
