58cf1e650aa11a69556e26d9c26d4c2edd32c06c
philippe.docourt <philippe@docourt.ch>
2020-04-26 15:59:49 +0200

    Update copyright in doc.

--- docs/conf.py
+++ docs/conf.py
@@ -72,7 +72,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Django QR Code'
-copyright = '2017-2018, Philippe Docourt'
+copyright = '2017-2020, Philippe Docourt'
 author = 'Philippe Docourt'
 
 # The version info for the project you're documenting, acts as replacement for
