88378611d363aedeaaf4acc859b82540f228359f
philippe.docourt <philippe@docourt.ch>
2020-04-26 17:30:13 +0200

    Make sure that we use an up to date version of setuptools, wheel, and twine when publishing package.

--- scripts/generate-pypi-release.sh
+++ scripts/generate-pypi-release.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 (
+    python -m pip install --upgrade pip
+    pip install --upgrade setuptools wheel twine
     cd ../
     rm -r build/ dist/ django_qr_code.egg-info/
     python setup.py check && python setup.py sdist && python setup.py bdist_wheel && twine upload dist/*
