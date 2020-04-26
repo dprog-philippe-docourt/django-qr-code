82c6e7582eaa4264a88128a8bf37dcb5f8c83fd7
philippe.docourt <philippe@docourt.ch>
2020-04-26 15:28:34 +0200

    Fixed broken run-demo-app.sh

--- scripts/run-demo-app.sh
+++ scripts/run-demo-app.sh
@@ -5,7 +5,7 @@
     DOCKER_COMPOSE_COMMAND='docker-compose -f docker-compose.yml'
 
     echo --- Build stage test container
-    ${DOCKER_COMPOSE_COMMAND} stop && ${DOCKER_COMPOSE_COMMAND} build
+    ${DOCKER_COMPOSE_COMMAND} stop && ${DOCKER_COMPOSE_COMMAND} --build-arg PYTHON_VERSION=3.8 build
 
     echo --- Fire up staging site
     ${DOCKER_COMPOSE_COMMAND} up -d --remove-orphans
