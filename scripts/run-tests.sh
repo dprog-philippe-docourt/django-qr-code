85498c459656b1ca2d5cc29798ac96a963198fe5
philippe.docourt <philippe@docourt.ch>
2020-11-22 12:51:45 +0100

    Add Python 3.9 to tested platform for local testing. Re-introduce Python 3.6 and 3.7 to test matrix for Travis.

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -14,7 +14,7 @@ log_file_name=log.txt
     echo "--- CPU: $(nproc --all)"
     echo "--- RAM: $(free -h)"
 
-    python_versions=("3.6 3.7 3.8")
+    python_versions=("3.6 3.7 3.8 3.9")
     django_versions=("2.2.17" "3.0.11" "3.1.3")
 
     for python_version in ${python_versions[@]}
