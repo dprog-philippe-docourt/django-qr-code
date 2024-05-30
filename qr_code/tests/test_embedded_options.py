9ecb164d6804943bcd0f0e188a26aebc47165d0d
philippe.docourt <philippe@docourt.ch>
2024-05-30 12:55:33 +0200

    Add tests for use_data_uri_for_svg argument when generating embedded SVG QR code.

--- qr_code/tests/test_embedded_options.py
+++ qr_code/tests/test_embedded_options.py
@@ -61,3 +61,27 @@ class TestQREmbeddedImageResult(SimpleTestCase):
             self.assertEqual(qr1, qr2)
             self.assertEqual(qr1, qr3)
             self.assertEqual(qr1, get_base64_svg_image_template(alt_text) % base64.b64encode(result.encode("utf-8")).decode("utf-8"))
+
+    def test_embedded_data_uri_svg(self):
+        base_ref_file_name = "qrfromtext_embedded_data_uri"
+        for use_data_uri_for_svg in [True, False]:
+            alt_text_name = str(use_data_uri_for_svg).lower()
+            print("Testing SVG with data URI: %s" % alt_text_name)
+            result_file_name = f"{base_ref_file_name}_{alt_text_name}"
+            qr1 = make_embedded_qr_code(TEST_TEXT, QRCodeOptions(image_format="svg"), use_data_uri_for_svg=use_data_uri_for_svg)
+            qr2 = qr_from_text(TEST_TEXT, image_format="svg", use_data_uri_for_svg=use_data_uri_for_svg)
+            qr3 = qr_from_text(TEST_TEXT, options=QRCodeOptions(image_format="svg"), use_data_uri_for_svg=use_data_uri_for_svg)
+            if REFRESH_REFERENCE_IMAGES:
+                if use_data_uri_for_svg:
+                    match = IMAGE_TAG_BASE64_DATA_RE.search(qr1)
+                    source_image_data = match.group("data")
+                    write_svg_content_to_file(result_file_name, base64.b64decode(source_image_data).decode("utf-8"))
+                else:
+                    write_svg_content_to_file(result_file_name, qr1)
+            result = get_svg_content_from_file_name(result_file_name)
+            self.assertEqual(qr1, qr2)
+            self.assertEqual(qr1, qr3)
+            if use_data_uri_for_svg:
+                self.assertEqual(qr1, get_base64_svg_image_template() % base64.b64encode(result.encode("utf-8")).decode("utf-8"))
+            else:
+                self.assertEqual(qr1, result)
