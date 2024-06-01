4e9e80f8686a123e4199681792084ef48c0993c1
philippe.docourt <philippe@docourt.ch>
2024-06-01 14:42:18 +0200

    Disable pseudo TTY for GitHub runners and run mypy check for each version of python.

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -10,10 +10,6 @@ fi
 mkdir -p "${output_folder_name}"
 log_file_name=log.txt
 
-echo "--- Run type checking"
-pip install -r requirements-dev.txt
-mypy qr_code || (echo "mypy" >> ${output_folder_name}/fail.flag)
-
 if [ $? -ne 0 ]; then
     echo
     echo --- Failed type checking. Abort tests!
@@ -35,33 +31,41 @@ django_versions=("4.2.13 5.0.6")
 for python_version in ${python_versions[@]}
 do
     DOCKER_COMPOSE_COMMAND="docker-compose -f docker-compose.yml"
+    if [ -n "$GITHUB_ACTION" ]; then
+        DOCKER_COMPOSE_EXEC_COMMAND="${DOCKER_COMPOSE_COMMAND} exec -T django-qr-code"
+    else
+        DOCKER_COMPOSE_EXEC_COMMAND="${DOCKER_COMPOSE_COMMAND} exec django-qr-code"
+    fi
+
+    ${DOCKER_COMPOSE_COMMAND} down
+    build_cmd="${DOCKER_COMPOSE_COMMAND} build --build-arg PYTHON_VERSION=${python_version}"
+    echo --- Build test container for Python ${python_version}: "${build_cmd}"
+    ${build_cmd}
+
+    ${DOCKER_COMPOSE_COMMAND} up -d
+
     for django_version in ${django_versions[@]}
     do
-        ${DOCKER_COMPOSE_COMMAND} down
-        build_cmd="${DOCKER_COMPOSE_COMMAND} build --build-arg PYTHON_VERSION=${python_version}"
-        echo --- Build test container for Python ${python_version}: "${build_cmd}"
-        ${build_cmd}
-
         echo --- Testing against: Python ${python_version} and Django ${django_version}
 
-        ${DOCKER_COMPOSE_COMMAND} up -d
-
         echo --- Force Django version
-        ${DOCKER_COMPOSE_COMMAND} exec django-qr-code pip install --upgrade "django~=${django_version}" || (echo "pip install - Python: ${python_version}, Django: ${django_version}" >> ${output_folder_name}/fail.flag)
+        ${DOCKER_COMPOSE_EXEC_COMMAND} pip install --upgrade "django~=${django_version}" || (echo "pip install - Python: ${python_version}, Django: ${django_version}" >> ${output_folder_name}/fail.flag)
         echo Output code for tests with Python ${python_version} and Django ${django_version}: $?
 
         echo --- Setup test environment
-        ${DOCKER_COMPOSE_COMMAND} exec django-qr-code python manage.py collectstatic --noinput || (echo "collectstatic - Python: ${python_version}, Django: ${django_version}" >> ${output_folder_name}/fail.flag)
+        ${DOCKER_COMPOSE_EXEC_COMMAND} python manage.py collectstatic --noinput || (echo "collectstatic - Python: ${python_version}, Django: ${django_version}" >> ${output_folder_name}/fail.flag)
 
         # Run tests
-        (${DOCKER_COMPOSE_COMMAND} exec django-qr-code python -Wd manage.py test || (echo "test - Python: ${python_version}, Django: ${django_version}" >> ${output_folder_name}/fail.flag)) | tee "${output_folder_name}/python_${python_version}-django_${django_version}-${log_file_name}"
-
-        echo --- Stop containers and remove them.
-        ${DOCKER_COMPOSE_COMMAND} down
+        (${DOCKER_COMPOSE_EXEC_COMMAND} python -Wd manage.py test || (echo "test - Python: ${python_version}, Django: ${django_version}" >> ${output_folder_name}/fail.flag)) | tee "${output_folder_name}/python_${python_version}-django_${django_version}-${log_file_name}"
      done
-done
 
-wait
+    echo "--- Run type checking"
+    ${DOCKER_COMPOSE_EXEC_COMMAND} pip install -r requirements-dev.txt || (echo "pip install - Python: ${python_version}" >> ${output_folder_name}/fail.flag)
+    ${DOCKER_COMPOSE_EXEC_COMMAND} mypy qr_code || (echo "mypy - Python: ${python_version}" >> ${output_folder_name}/fail.flag)
+
+    echo --- Stop containers and remove them.
+    ${DOCKER_COMPOSE_COMMAND} down
+done
 
 echo
 echo --- End of tests at $(date)
@@ -78,6 +82,9 @@ if [ -f "${output_folder_name}/fail.flag" ]; then
 else
   cat "${output_folder_name}/"*${log_file_name} | grep "FAILED" || success=true
 fi
+
+echo ""
+echo "--- Test results:"
 cat "${output_folder_name}/"*${log_file_name} | grep "Ran "
 cat "${output_folder_name}/"*${log_file_name} | grep -A 40 -B 1 "ERROR:"
 cat "${output_folder_name}/"*${log_file_name} | grep -A 20 -B 1 "FAILURE:"
